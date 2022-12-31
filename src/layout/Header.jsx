4fc87621adf94e9aaaab4743d0e428f11512373d
Павел <pasha.seregin.92@mail.ru>
2022-12-31 15:55:23 +0300

    ref

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -1,12 +1,21 @@
 export const Header = () => (
   <nav className='deep-purple lighten-1'>
     <div className='nav-wrapper'>
-      <a href='!#' className='brand-logo'>
+      <a href='/' className='brand-logo'>
         React Movies
       </a>
-      <ul id='nav-mobile' className='right hide-on-med-and-down'>
+      <ul
+        id='nav-mobile'
+        className='right hide-on-med-and-down'
+      >
         <li>
-          <a href='!#'>Repo</a>
+          <a
+            href='https://github.com/hangryRAY123/react-movies'
+            target='_blank'
+            rel='noreferrer'
+          >
+            Repo
+          </a>
         </li>
       </ul>
     </div>
